49527f599489a14fed7a5dbb19631eeefe9b39ab
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-11 12:10:31 -0600

    add origin ca configuration

--- main.tf
+++ main.tf
@@ -7,3 +7,28 @@ module "submodule" {
 
   message = "Hello, submodule"
 }
+
+#
+# Origin CA Issuer 
+#
+
+resource "kubernetes_namespace" "origin_ca_issuer" {
+  metadata {
+    name = var.namespace_name
+  }
+}
+
+resource "helm_release" "origin_ca_issuer" {
+  name       = var.helm_release_name
+  repository = "oci://ghcr.io/cloudflare/origin-ca-issuer-charts/"
+  chart      = "origin-ca-issuer"
+  version    = var.helm_chart_version
+  namespace  = var.namespace_name
+
+  values     = [file("${path.module}/values.yaml")]
+
+  # set {
+  #   name  = "certmanager.namespace"
+  #   value = "ccp-core"
+  # }
+}
--- variables.tf
+++ variables.tf
@@ -23,3 +23,24 @@ EOF
   type        = map(any)
   default     = {}
 }
+
+#
+# External DNS
+#
+
+variable "namespace_name" {
+  description = "The namespace where resources will be created, it have to be the same as cert-manager."
+  type        = string
+}
+
+variable "helm_release_name" {
+  description = "The name of the Helm release."
+  type        = string
+  default     = "origin-ca-issuer"
+}
+
+variable "helm_chart_version" {
+  description = "The version of the Helm chart."
+  type        = string
+  default     = "0.5.5"
+}
