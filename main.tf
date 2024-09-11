dbb4644f6da21aea5ec59b7f813e1b0cc7b40606
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-11 13:50:45 -0600

    remove namespace resource

--- main.tf
+++ main.tf
@@ -12,29 +12,6 @@ module "submodule" {
 # Origin CA Issuer 
 #
 
-resource "kubernetes_namespace" "this" {
-  count = length(
-    try(
-      [for ns in data.kubernetes_namespace.existing.namespaces : ns.metadata.name if ns.metadata.name == var.namespace_name],
-      []
-    )
-  ) == 0 ? 1 : 0
-
-  metadata {
-    name = var.namespace_name
-  }
-}
-
-data "kubernetes_namespace" "existing" {
-  for_each = {
-    for ns in kubernetes_namespace.this : ns.metadata.name => ns
-  }
-
-  metadata {
-    name = var.namespace_name
-  }
-}
-
 resource "helm_release" "this" {
   name       = var.helm_release_name
   repository = "oci://ghcr.io/cloudflare/origin-ca-issuer-charts/"
