8f426b181acd8b5284244f10f050b74f43f649af
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-16 23:52:20 -0600

    add values

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@
 # Install CRDs
 #
 
-resource "null_resource" "apply_originissuer" {
+resource "null_resource" "crd_originissuer" {
   provisioner "local-exec" {
     command = "kubectl apply -f https://raw.githubusercontent.com/cloudflare/origin-ca-issuer/${var.crds_version}/deploy/crds/cert-manager.k8s.cloudflare.com_originissuers.yaml"
   }
 }
 
-resource "null_resource" "apply_originissuer" {
+resource "null_resource" "crd_clusteroriginissuer" {
   provisioner "local-exec" {
-    command = "kubectl apply -f https://raw.githubusercontent.com/cloudflare/origin-ca-issuer/${var.crds_version}/deploy/crds/cert-manager.k8s.cloudflare.com_originissuers.yaml"
+    command = "kubectl apply -f https://raw.githubusercontent.com/cloudflare/origin-ca-issuer/${var.crds_version}/deploy/crds/cert-manager.k8s.cloudflare.com_clusteroriginissuers.yaml"
   }
 }
 
@@ -18,24 +18,30 @@ resource "null_resource" "apply_originissuer" {
 # Origin CA Issuer 
 #
 
-# resource "helm_release" "this" {
-#   name       = var.helm_release_name
-#   repository = "oci://ghcr.io/cloudflare/origin-ca-issuer-charts/"
-#   chart      = "origin-ca-issuer"
-#   version    = var.helm_chart_version
-#   namespace  = var.namespace_name
+resource "helm_release" "this" {
+  name       = var.helm_release_name
+  repository = "oci://ghcr.io/cloudflare/origin-ca-issuer-charts/"
+  chart      = "origin-ca-issuer"
+  version    = var.helm_chart_version
+  namespace  = var.namespace_name
 
-#   values = [
-#     templatefile("${path.module}/values.yaml.tpl", {
-#       harbor_admin_password = var.harbor_admin_password,
-#       domain_name           = var.domain_name,
-#       dash_domain_name      = var.dash_domain_name,
-#       environment           = var.environment
+  # values     = [file("${path.module}/values.yaml")]
 
-#     })
-#   ]
-# }
+  # values = [
+  #   templatefile("${path.module}/values.yaml.tpl", {
+  #     harbor_admin_password = var.harbor_admin_password,
+  #     domain_name           = var.domain_name,
+  #     dash_domain_name      = var.dash_domain_name,
+  #     environment           = var.environment
 
-resource "kubernetes_manifest" "origin_ca" {
-  manifest = yamldecode(file("${path.module}/value.yaml"))
+  #   })
+  # ]
+  set {
+    name  = "global.rbac.create"
+    value = "true"
+  }
 }
+
+# resource "kubernetes_manifest" "origin_ca" {
+#   manifest = yamldecode(file("${path.module}/value.yaml"))
+# }
