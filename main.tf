4e93ac6aa499b742b2682fd0f65c130128b46d00
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-16 23:26:31 -0600

    add origin ca changes

--- main.tf
+++ main.tf
@@ -1,28 +1,41 @@
-locals {
-  context = var.context
+#
+# Install CRDs
+#
+
+resource "null_resource" "apply_originissuer" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/cloudflare/origin-ca-issuer/${var.crds_version}/deploy/crds/cert-manager.k8s.cloudflare.com_originissuers.yaml"
+  }
 }
 
-module "submodule" {
-  source = "./modules/submodule"
-
-  message = "Hello, submodule"
+resource "null_resource" "apply_originissuer" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/cloudflare/origin-ca-issuer/${var.crds_version}/deploy/crds/cert-manager.k8s.cloudflare.com_originissuers.yaml"
+  }
 }
 
 #
 # Origin CA Issuer 
 #
 
-resource "helm_release" "this" {
-  name       = var.helm_release_name
-  repository = "oci://ghcr.io/cloudflare/origin-ca-issuer-charts/"
-  chart      = "origin-ca-issuer"
-  version    = var.helm_chart_version
-  namespace  = var.namespace_name
+# resource "helm_release" "this" {
+#   name       = var.helm_release_name
+#   repository = "oci://ghcr.io/cloudflare/origin-ca-issuer-charts/"
+#   chart      = "origin-ca-issuer"
+#   version    = var.helm_chart_version
+#   namespace  = var.namespace_name
 
-  # values     = [file("${path.module}/values.yaml")]
+#   values = [
+#     templatefile("${path.module}/values.yaml.tpl", {
+#       harbor_admin_password = var.harbor_admin_password,
+#       domain_name           = var.domain_name,
+#       dash_domain_name      = var.dash_domain_name,
+#       environment           = var.environment
 
-  # set {
-  #   name  = "certmanager.namespace"
-  #   value = "ccp-core"
-  # }
+#     })
+#   ]
+# }
+
+resource "kubernetes_manifest" "origin_ca" {
+  manifest = yamldecode(file("${path.module}/value.yaml"))
 }
--- variables.tf
+++ variables.tf
@@ -1,31 +1,15 @@
 #
-# Contextual Fields
+# CRDs variables
 #
 
-variable "context" {
-  description = <<-EOF
-Receive contextual information. When Walrus deploys, Walrus will inject specific contextual information into this field.
-
-Examples:
-```
-context:
-  project:
-    name: string
-    id: string
-  environment:
-    name: string
-    id: string
-  resource:
-    name: string
-    id: string
-```
-EOF
-  type        = map(any)
-  default     = {}
+variable "crds_version" {
+  description = "Version of the OriginIssuer CRD"
+  type        = string
+  default     = "v0.9.0"
 }
 
 #
-# External DNS
+# External DNS variables
 #
 
 variable "namespace_name" {
