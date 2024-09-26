032081a6054a78b3975347f2fdac40bd25e49de5
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-25 22:42:38 -0600

    add provider

--- versions.tf
+++ versions.tf
@@ -10,5 +10,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.23.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
   }
 }
