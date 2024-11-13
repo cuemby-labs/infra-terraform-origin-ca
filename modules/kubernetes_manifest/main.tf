b613cfbb8e2d61d5b6f5cd439779e972e5d1b8a9
Manuel Nuila <manuel.nuila@cuemby.com>
2024-11-12 23:24:44 -0600

    test

--- modules/kubernetes_manifest/main.tf
+++ modules/kubernetes_manifest/main.tf
@@ -2,22 +2,16 @@
 # Manifests resoruces
 #
 
-data "http" "manifest_url" {
- # for_each = toset(var.manifests_urls)
-  for_each = { for file, url in var.manifests_urls : file => url }
+provider "http" {}
+
+data "http" "manifests" {
+  for_each = toset(var.manifests_urls)
   url      = each.value
 }
 
-# data "kubectl_file_documents" "manifest_files" {
-#   for_each = data.http.manifest_url
-#   content  = each.value.response_body
-# }
-
-resource "kubectl_manifest" "install_manifest_files" {
-  # for_each  = { for key, doc in data.kubectl_file_documents.manifest_files : key => doc.documents }
-  for_each = { for file, url in var.manifests_urls : file => url }
-  # yaml_body = join("\n---\n", each.value)
-  yaml_body = data.http.manifest_url[each.key].response_body
+resource "kubectl_manifest" "apply_manifests" {
+  for_each  = toset(var.manifests_urls)
+  yaml_body = yamldecode(data.http.manifests[each.value].body)
 }
 
 #
