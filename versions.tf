67a6e11489dc14ec2e0e9574e3118f830c290aec
Manuel Nuila <manuel.nuila@cuemby.com>
2024-11-24 22:56:45 -0600

    test

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.5.7"
 
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.11.0"
+    template = {
+      source = "hashicorp/template"
+      version = ">= 2.2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
